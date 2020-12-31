f736362e981bf4f4415a62ea9977cc47ae0aa8ef
Sleather Vega <sleatherv28@hotmail.com>
2020-12-30 21:09:34 -0500

    Implement Remember session

--- index.js
+++ index.js
@@ -14,7 +14,11 @@ app.use(cookieParser());
 
 //Basic Strategy
 require('./utils/auth/strategies/basic');
+const THIRTY_DAYS_IN_SEC = 2592000;
+const TWO_HOURS_IN_SEC = 7200;
 app.post("/auth/sign-in", async function (req, res, next) {
+    // Obtenemos el atributo rememberMe desde el cuerpo del request
+    const { rememberMe } = req.body;
     passport.authenticate('basic', function (error, data) {
         try {
             if (error || data) {
@@ -25,9 +29,12 @@ app.post("/auth/sign-in", async function (req, res, next) {
                     next(err);
                 }
                 const { token, ...user } = data;
+                // Si el atributo rememberMe es verdadero la expiración será en 30 dias
+                // de lo contrario la expiración será en 2 horas
                 res.cookie("token", token, {
                     httpOnly: !config.dev,
-                    secure: !config.dev
+                    secure: !config.dev,
+                    maxAge: rememberMe ? THIRTY_DAYS_IN_SEC : TWO_HOURS_IN_SEC
                 });
             });
             res.status(200), json(user);

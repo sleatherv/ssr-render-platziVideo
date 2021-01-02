49e16b1a839b9e1af567250a78bf97ec80875543
Sleather Vega <sleatherv28@hotmail.com>
2021-01-01 19:43:57 -0500

    Auth with google OoenID connect

--- index.js
+++ index.js
@@ -140,6 +140,31 @@ app.get(
         res.status(200).json(user);
     }
 );
+app.get(
+    "/auth/google",
+    passport.authenticate("google", {
+        scope: ["email", "profile", "openid"]
+    })
+);
+
+app.get(
+    "/auth/google/callback",
+    passport.authenticate("google", { session: false }),
+    function (req, res, next) {
+        if (!req.user) {
+            next(boom.unauthorized());
+        }
+
+        const { token, ...user } = req.user;
+
+        res.cookie("token", token, {
+            httpOnly: !config.dev,
+            secure: !config.dev
+        });
+
+        res.status(200).json(user);
+    }
+);
 
 app.listen(config.port, function () {
     console.log(`Listening http://localhost:${config.port}`);
